e7e87bd0f7f86d463945fdf469331cda527e28c1
Nenad Milutinovic <fhb201257@fh-vie.ac.at>
2023-01-21 22:00:04 +0100

    Home / Übersicht für Token erstellt (Token sind gemockt)

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
+  const [token, setToken] = useState([
+    { title: 'Bitcoine', value: 2134.92, id: 1 },
+    { title: 'Etherium', value: 980.11, id: 2 },
+    { title: 'Dogcoin', value: 1.52, id: 3 }
+  ])
+
+  return (
+    <div className="home">
+      {token.map(token => (
+        <div className="token-preview" key={token.id} >
+          <h2>{ token.title }</h2>
+          <h3>Current value is   ${ token.value }</h3>
         </div>
-     );
+      ))}
+    </div>
+  );
 }
  
 export default Home;
