fe342ff8a1e3b858bdf1f8e3eb1c11378fb2d92a
Nenad Milutinovic <fhb201257@fh-vie.ac.at>
2023-01-21 17:09:09 +0100

    Test1

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new site';
+  const likes = 54;
+ // const person = { name: 'Brian', age: 32 };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="content">
+      <h1>App Component</h1>
+      <h2>{title}</h2>
+      <p>Liked {likes} times</p>
+
+      <p>{ Math.random() * 10 }</p>
+
+     </div>
     </div>
   );
 }
