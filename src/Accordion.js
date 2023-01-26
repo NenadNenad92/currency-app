d4b3ac06e8d9181d71953cea20df490e75ce87cf
Nenad Milutinovic <fhb201257@fh-vie.ac.at>
2023-01-26 22:47:08 +0100

    CSS-Style Änderungen

--- src/Accordion.js
+++ src/Accordion.js
@@ -5,15 +5,16 @@ const Accordion = ({name, active, setActive, price_usd, asset_id, data_end }) =>
 
     //Accordion How-To: https://www.youtube.com/watch?v=gs14LEv7Hfk&t=288s
     return (
+        
         <div className="accordion">
            <div className="accordionHeader">
-                <div className="container">
+                <div className="container-title">
                     <p>{name}</p>
                     <span onClick={() => setActive(name)}> {active === name ? "X" : "|||"} </span>
                 </div>
             </div> 
             <div className={(active === name ? "show" : "")  + "accordionContent"}>
-                <div className="container">
+                <div className="container-detail">
                     <p>Aktueller Wert: € {price_usd}</p> <br></br>
                     <p>Code: {asset_id}</p> <br></br>
                     <p>Datum der Abfrage: {data_end} </p>
